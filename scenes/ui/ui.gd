0fea2db355e07b76fc960180a6495b60a77709f6
Nico Viarnes <nico@viarn.es>
2023-05-22 21:52:15 -0700

    flesh out ui

--- scenes/ui/ui.gd
+++ scenes/ui/ui.gd
@@ -13,3 +13,7 @@ func _process(delta):
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
+
+
+func _on_settings_pressed():
+	pass # Replace with function body.
